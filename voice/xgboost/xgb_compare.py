cfa9482875dc24b4181fb4b33f546a302ab2a988
abhijith <abhijith@pop-os.localdomain>
2023-02-06 18:50:00 +0530

    first commit

---
+++ voice/xgboost/xgb_compare.py
@@ -0,0 +1,82 @@
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+# load data
+def importdata():
+	balance_data = pd.read_csv('../compare_adcn.csv',sep= ',', header = None)
+	
+	# Printing the dataswet shape
+	print ("Dataset Length: ", len(balance_data))
+	print ("Dataset Shape: ", balance_data.shape)
+	
+	# Printing the dataset obseravtions
+	print ("Dataset: ",balance_data.head())
+	return balance_data
+
+def splitdataset(balance_data):
+
+	# Separating the target variable
+	X = balance_data.values[:, 0:6372]
+	Y = balance_data.values[:, 6373]
+
+	# Splitting the dataset into train and test
+	X_train, X_test, y_train, y_test = train_test_split(
+	X, Y, test_size = 0.3, random_state = 100)
+	
+	return X, Y, X_train, X_test, y_train, y_test
+
+def train_using_xgb(X_train, X_test, y_train):
+
+	# Creating the classifier object
+    model =XGBClassifier(max_depth=12,n_estimators=300,learning_rate = 1) 
+
+    model.fit(X_train, y_train)
+    return model
+
+	
+def prediction(X_test, clf_object):
+
+	# Predicton on test with giniIndex
+	y_pred = clf_object.predict(X_test)
+	print("Predicted values:")
+	print(y_pred)
+	return y_pred
+
+def cal_accuracy(y_test, y_pred):
+	
+	print("Confusion Matrix: ",
+		confusion_matrix(y_test, y_pred))
+	
+	print ("Accuracy : ",
+	accuracy_score(y_test,y_pred)*100)
+	
+	print("Report : ",
+	classification_report(y_test, y_pred))
+
+def main():
+	
+	# Building Phase
+	data = importdata()
+	X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+	clf_xgb = train_using_xgb(X_train, X_test, y_train)
+	
+	
+	# Operational Phase
+	print("Results Using xgb Index:")
+	
+	# Prediction using gini
+	y_pred_xgb = prediction(X_test, clf_xgb)
+	cal_accuracy(y_test, y_pred_xgb)
+	
+	
+	
+	
+# Calling main function
+if __name__=="__main__":
+	main()
+
+# make predictions for test data
